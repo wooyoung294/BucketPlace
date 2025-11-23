4ee3490e090afdb32a480744224528707c9a4cda
wooyoung.choi <160103260+wooyoung294@users.noreply.github.com>
2025-11-23 19:12:11 +0900

    Merge pull request #2 from wooyoung294/BKP-002
    * feat(conf): conftest.py 작성
    
    * fix(conf): pytest fixtures name

---
+++ todays_house/conftest.py
@@ -0,0 +1,44 @@
+import base64
+
+import allure
+from appium import webdriver
+from appium.options.android import UiAutomator2Options
+import pytest
+
+
+@pytest.fixture
+def app_driver():
+    app_capabilities = {
+        'platformName': 'Android',
+        'automationName': 'UiAutomator2',
+        'appPackage': 'com.android.vending',
+        'uiautomator2ServerLaunchTimeout': 120000,
+        'uiautomator2ServerInstallTimeout': 120000,
+        'uiautomator2ServerReadTimeout': 120000,
+        'adbExecTimeout': 120000,
+        'newCommandTimeout': 10800,
+        'skipServerInstallation': False,
+    }
+
+    appium_server_url = 'http://localhost:4723'
+    driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(app_capabilities))
+
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture(autouse=True)
+def recorded_app_driver(request, app_driver):
+    app_driver.start_recording_screen()
+    try:
+        yield app_driver
+    finally:
+        app_driver.remove_app('net.bucketplace')
+        video_data = base64.b64decode(app_driver.stop_recording_screen())
+
+        allure.attach(video_data, name=f'{request.node.name}', attachment_type=allure.attachment_type.WEBM)
+
+
+@pytest.fixture
+def todays_house_values():
+    return {}
